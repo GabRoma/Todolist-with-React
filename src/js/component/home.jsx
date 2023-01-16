2a62d9552499d309f8e7bd81081bd9f0fda87a26
GabRoma <gabrielrg901@gmail.com>
2023-01-16 15:56:37 +0000

    fetch attempt two

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,40 +2,33 @@ import React, { useState, useEffect } from "react";
 
 const Home = () => {
 
-  useEffect(() => {
-    fetch('https://assets.breatheco.de/apis/fake/todos/user/theuser',{
-      method: "GET",
-    })
-    .then(response => response.text())
-    .then(result => console.log(result))
-  });
+useEffect(() =>
+fetch('https://assets.breatheco.de/apis/fake/todos/user/theuser')
+    .then((response)=>response.json())
+    .then((data) => setTasks(data.map(d => d.label)))
+    .then((data)=>console.log(data))
+,[])   
+
+useEffect(() =>
+fetch('https://assets.breatheco.de/apis/fake/todos/user/theuser', {
+   method: "PUT",
+   body: JSON.stringify(tasks.map(it => ({ label: it, done: false }))),
+   headers: {
+     "Content-Type": "application/json"
+   }
+ }, [tasks]))
 
   const [input, setInput] = useState("");
   const [tasks, setTasks] = useState([]);
-  const tasksList = tasks.map((item)=>{
-    return {
-       "label": item,
-       "done": false
-   }
-  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (input !== "") {
-        setTasks([...tasks, input])
+        setTasks(tasks.concat(input))
         setInput("");
     }
-    fetch('https://assets.breatheco.de/apis/fake/todos/user/todouser', {
-        method: "PUT",
-        body: JSON.stringify(tasksList),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        // .then(response => response.text())
-        // .then(result => console.log(result))
-        // .catch(error => console.log('error', error));
-
 };
+
   const deleting = () => {
     setTasks([]);
   };
@@ -43,7 +36,7 @@ const Home = () => {
   return (
     <>
       <div className="card mx-auto mt-4" style={{ width: 500, height: 600 }}>
-	  <div class="card-header">
+	  <div className="card-header">
     Things to do
   </div>
 		<div className="card-body">
@@ -64,18 +57,22 @@ const Home = () => {
           </button>
         </form>
         <div>
-			{tasks.map((item, index) => (
-				<div key={index} className="row d-flex p-1 mt-2 border border-light rounded">
-					<div className="col-6">{item}{""}</div>
-					<div className="col-6 text-end">
-					<i className="fas fa-check"
+        <ul className="list-group">
+                    {tasks.map((item, index) => (
+                        <li key={index} className="list-group-item d-flex justify-content-between">
+                                {item}
+                            <button
+                                type="button"
+                                className="btn-close"
                                 onClick={
-                                    () => setTasks(tasks.filter((_, currentIndex) => {
-                                      return index != currentIndex}))
-                            }></i>
-					</div>
-				</div>
-			))}
+                                  () => setTasks(tasks.filter((_, currentIndex) => {
+                                    return index != currentIndex}))
+                          }
+                            >
+                            </button>
+                        </li>
+                    ))}
+                </ul>
 			</div>
 			</div>
 		<div className="card-footer">
